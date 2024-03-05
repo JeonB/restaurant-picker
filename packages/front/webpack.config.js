1f437e03eb194177374583a67c8a7b8273ddc8ec
JeonB <newjeonb@gmail.com>
2024-03-05 15:43:00 +0900

    Add ts-node & Remove envKeys Pluugin

--- packages/front/webpack.config.js
+++ packages/front/webpack.config.js
@@ -1,19 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 const webpack = require('webpack');
-const dotenv = require('dotenv');
 
 const isLocal = process.env.NODE_ENV !== 'development';
 const host = isLocal ? 'localhost' : '192.168.10.100';
 
-const env = dotenv.config().parsed;
-
-// 환경 변수를 문자열로 변환하여 DefinePlugin에 전달
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 module.exports = {
   output: {
     publicPath: `http://${host}:3002/`,
@@ -88,6 +79,5 @@ module.exports = {
       template: './public/index.html',
     }),
     new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin(envKeys),
   ],
 };
