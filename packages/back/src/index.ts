f38ff31ccc30a6bf8bf9aa7580850a844d183f3f
sojeon <sojeon@ifinfo.co.kr>
2024-03-21 18:00:13 +0900

    Add Fastify Swagger and Swagger UI

--- packages/back/src/index.ts
+++ packages/back/src/index.ts
@@ -9,14 +9,83 @@ import restaurant from './modules/restaurant/router';
 import user from './modules/user/router';
 import db from './decorators/db';
 import auth from './middlewares/auth';
-
+import swagger from '@fastify/swagger';
+import test from './modules/restaurant/test';
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
   fastify({ logger: true });
 
+server.register(swagger, {
+  openapi: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Test swagger',
+      description: 'Testing the Fastify swagger API',
+      version: '0.1.0',
+    },
+
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+    tags: [
+      { name: 'user', description: 'User related end-points' },
+      { name: 'code', description: 'Code related end-points' },
+    ],
+    components: {
+      securitySchemes: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header',
+        },
+      },
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here',
+    },
+  },
+});
 server.register(restaurant);
 server.register(user);
 server.register(db);
 server.register(auth);
+server.register(test);
+server.register(require('@fastify/swagger-ui'), {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (
+      request: FastifyRequest,
+      reply: FastifyReply,
+      next: () => void,
+    ) {
+      next();
+    },
+    preHandler: function (
+      request: FastifyRequest,
+      reply: FastifyReply,
+      next: () => void,
+    ) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header: any) => header,
+  transformSpecification: (
+    swaggerObject: any,
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
 server.listen({
   host: '0.0.0.0', //외부에서 접속 가능
   port: 3000,
