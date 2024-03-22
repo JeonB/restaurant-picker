4d75ab06453e0e2f2155c31a40f3a153fe669089
sojeon <sojeon@ifinfo.co.kr>
2024-03-22 17:57:12 +0900

    Add dotenv-webpack and MapComponent

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -4,11 +4,13 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import { RandomPicker } from './components/randomPicker';
+import { MapComponent } from './components/map';
 
 const App: React.FC = () => {
   return (
     <div className="text-center">
       <RandomPicker />
+      <MapComponent />
     </div>
   );
 };
