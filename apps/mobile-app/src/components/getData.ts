d5c79e71d7f6dd6b24257a3de4165b7f598eb28b
sojeon <sojeon@ifinfo.co.kr>
2024-04-01 16:01:25 +0900

    Delete unused files and configurations

--- apps/mobile-app/src/components/getData.ts
+++ apps/mobile-app/src/components/getData.ts
@@ -1,19 +1,19 @@
-import { Restaurant } from "@projectTypes";
+import { Restaurant } from '@projectTypes';
 export async function getData(category: string) {
-  const baseUrl = "http://192.168.10.108:3000/restaurants";
+  const baseUrl = 'http://192.168.10.186:3000/restaurants';
   try {
     const url = `${baseUrl}/${category}`;
     const response = await fetch(url, {
-      method: "GET",
+      method: 'GET',
     });
 
     if (!response.ok) {
-      throw new Error("Response 실패");
+      throw new Error('Response 실패');
     }
     const data: Restaurant[] = await response.json();
 
     if (data.length === 0) {
-      console.log("데이터가 없습니다.");
+      console.log('데이터가 없습니다.');
       return;
     }
     // console.log(data);
@@ -22,6 +22,6 @@ export async function getData(category: string) {
     // randomPlaceName.distance += " m";
     return data;
   } catch (error) {
-    console.error("Error occurred:", error);
+    console.error('Error occurred:', error);
   }
 }
